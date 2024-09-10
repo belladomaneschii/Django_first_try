d0ca961a004817ace252e0e0eafa3284315ce3ff
Bella Domaneschi <isabella.domaneschi@gmail.com>
2024-09-10 18:29:26 +1200

    Add files via upload

---
+++ views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .models import Project
+projectsList = [
+
+            {'id': '1',
+             'title': 'Ecommerce Website', 
+             'description': 'Fully functional ecommerce website' },
+
+            { 'id': '2',
+              'title': 'Portfolio Website',
+              'description': 'A personal website to write articles and display work' },
+
+            {'id': '3',
+             'title': 'Social Network',
+             'description': 'An open source project built by the community' }
+
+]
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    print('projectObj:', projectObj)
+  
+    return render(request, 'projects/single-project.html', {'project':projectObj})
